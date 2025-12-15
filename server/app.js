267b57a7bbc2ba4be9f970a45fc3833b8c87e4b2
Bhavishya chauhan <bhavishya2468@gmail.com>
2025-12-15 10:26:11 +0530

    changes added

--- server/app.js
+++ server/app.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 const router=require("./routes/router");
 require('dotenv').config();
+const cors=require("cors");
+const bodyParser=require("body-parser");
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.use(cors());
 app.use("/", router);
 
 
